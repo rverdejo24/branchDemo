694ad36dc1dea4650abc536ba20d280a131bb0d7
Rowell <lr.rverdejo@mmdc.mcl.edu.ph>
2026-04-19 17:46:57 +0800

    Add update

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     static void main(String[] args) {
         System.out.println("Hello from Java!");
         System.out.println("Rowell update.");
+        System.out.println("Rowell update.");
     }
 }
